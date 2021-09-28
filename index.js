0a615666a55aa6fc04769ede25a1e20796cf689c
Michael <mjpj@live.com>
2021-09-27 22:08:04 -0400

    commit tree efb1b9d77257c48acbeb52db7743f7fa7c9733d0

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-
-// Write your code here
 const num1 = 2;
 const num2 = 31;
 const multiply = num1 * num2;
-const random = Math.floor(Math.random() * 10) + 1;
-const num3 = 16;
-const num4 = 6;
-const mod = num3 % num4;
-const max = Math.max(7, 10, 12, 20);
 
 
 
+const random = Math.floor(Math.random() + 1)
 
-// const num1 = 2;
-// const num2 = 31;
-// const multiply = num1 * num2;
-// const random = Math.floor( Math.random() * 1000 ) + 1;
-// const num3 = 16;
-// const num4 = 6;
-// const mod = num3 % num4;
-// const max = Math.max( 5, 10, 15, 20 );
+let num3 = 16
+let num4 = 6
+let remainder = num3/num4;
+const max = Math.max(1,2,3,20)
